2de8f5d047a7797916dc2e50794edc32be01d31c
Vasily Levit <vasily.levit@gmail.com>
2022-09-15 12:11:49 +0800

    modified home task2

--- homework2.py
+++ homework2.py
@@ -3,8 +3,8 @@
 # Пример: - [2, 3, 4, 5, 6] => [12, 15, 16];  - [2, 3, 5, 6] => [12, 15]
 
 input_list = [2, 3, 4, 5, 6]
-out_list = []
+result = []
 len_list = len(input_list)
-for i in range(0, len_list - len_list // 2):
-    out_list.append(input_list[i] * input_list[-i-1])
-print(out_list)
+for i in range(len_list - len_list // 2):
+    result.append(input_list[i] * input_list[-i-1])
+print(result)
