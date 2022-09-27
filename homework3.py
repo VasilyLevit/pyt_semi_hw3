4fc2e70f6cc3d6a08848fc12f1e749bd74555afc
Vasily Levit <vasily.levit@gmail.com>
2022-09-27 12:08:35 +0800

    add comprehension in hometask3-3

--- homework3.py
+++ homework3.py
@@ -3,12 +3,16 @@
 # Пример: - [1.1, 1.2, 3.1, 10.01] => 0.19
 
 in_list = [1.1, 1.2, 3.1, 10.01]
-max = min = in_list[0]%1
+# Вариант 1
+# max = min = in_list[0]%1
 
-for i in in_list[1::]:
-    if (i%1) > max:
-        max = i%1
-    elif (i%1) < min:
-        min = i%1
+# for i in in_list[1::]:
+#     if (i%1) > max:
+#         max = i%1
+#     elif (i%1) < min:
+#         min = i%1
+# print('max-min = ', round(max-min,2))
 
-print('max-min = ', round(max-min,2))
+# Вариант 2 - с испльзованим LC от Тани
+list_2 = list(map(lambda x: (x * 100 % 100), in_list))
+print((max(list_2) - min(list_2)) / 100)
