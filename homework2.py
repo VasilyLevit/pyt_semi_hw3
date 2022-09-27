4eed2d3db108d35b20c3a4ddb40f1eac7662c130
Vasily Levit <vasily.levit@gmail.com>
2022-09-27 11:20:38 +0800

    add list comprehansion in hometask2

--- homework2.py
+++ homework2.py
@@ -3,8 +3,14 @@
 # Пример: - [2, 3, 4, 5, 6] => [12, 15, 16];  - [2, 3, 5, 6] => [12, 15]
 
 input_list = [2, 3, 4, 5, 6]
+# result = []
+# len_list = len(input_list)
+# for i in range(len_list - len_list // 2):
+#     result.append(input_list[i] * input_list[-i-1])
+# print(result)
+
 result = []
 len_list = len(input_list)
-for i in range(len_list - len_list // 2):
-    result.append(input_list[i] * input_list[-i-1])
-print(result)
+# result = [input_list[i] * input_list[-i-1] for i in range(len_list - len_list // 2)]
+# print(result)
+print(list(input_list[i] * input_list[-i-1] for i in range(len_list - len_list // 2)))
